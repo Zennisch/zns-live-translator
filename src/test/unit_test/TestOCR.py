f0c3f51ea63310c1b98256b79ce01a5d4ac9b4ca
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-06-15 01:25:41 +0900

    update: move test file location

--- src/test/unit_test/TestOCR.py
+++ src/test/unit_test/TestOCR.py
@@ -2,7 +2,7 @@ from core import OCR
 
 
 def test_extract_text_from_image_file_path():
-    image_path = "../asset/test/"
+    image_path = "../../asset/test/"
     text = OCR.extract_text_from_image(image_path, lang="vie")
     print(text)
     assert text != "", "OCR extraction failed, text is empty"
